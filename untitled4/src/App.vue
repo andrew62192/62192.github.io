a71a30eee1949a96b029da83084aaa9bd6a3323e
andrew62192 <96405778+andrew62192@users.noreply.github.com>
2023-05-30 23:50:36 +0800

    Add files via upload

---
+++ untitled4/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <shopping-list />
+</template>
+
+<script>
+import ShoppingList from './components/ShoppingList.vue';
+
+export default {
+  components: {
+    ShoppingList
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
